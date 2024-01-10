56c83a7edd6255147b30310327742710695ecdd1
offthestudrio <146812318+offthestudrio@users.noreply.github.com>
2024-01-11 03:18:15 +0800

    Add files via upload

---
+++ JS/21ST_CEN_Q2_M5.js
@@ -0,0 +1,168 @@
+let questions = [
+  {
+    numb: 1,
+    question: "A short story is interesting and fun to read because it teaches ______ or lesson.",
+    answer: "C. moral",
+    options: [
+      "A. biographical",
+      "B. political",
+      "C. moral",
+      "D. sociological"
+    ]
+  },
+  {
+    numb: 2,
+    question: "The main character in the story is known as the __________.",
+    answer: "A. protagonist",
+    options: [
+      "A. protagonist",
+      "B. antagonist",
+      "C. feminist",
+      "D. masculinist"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The most exciting part of a story is called _______________.",
+    answer: "B. climax",
+    options: [
+      "A. character",
+      "B. climax",
+      "C. conflict",
+      "D. plot"
+    ]
+  },
+  {
+    numb: 4,
+    question: "This element refers to the topic that the writer writes or comments on in his or her writing.",
+    answer: "D. theme",
+    options: [
+      "A. plot",
+      "B. conflict",
+      "C. climax",
+      "D. theme"
+    ]
+  },
+  {
+    numb: 5,
+    question: "It refers to the tension, the fight or the struggle between the various characters or forces in the story.",
+    answer: "A. conflict",
+    options: [
+      "A. conflict",
+      "B. theme",
+      "C. setting",
+      "D. plot"
+    ]
+  },
+  {
+    numb: 6,
+    question: "To produce a literary adaptation of a story, one needs to apply his ICT skills. ICT means _____.",
+    answer: "A. Information and Communication Technology",
+    options: [
+      "A. Information and Communication Technology",
+      "B. Information to Centralize Tutorial",
+      "C. Inclusive for Classroom Technology",
+      "D. Important in Communication Technology"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Information Technology (IT) is the use of __________ to store, retrieve, transmit, and manipulate data or information.",
+    answer: "C. computer",
+    options: [
+      "A. printer",
+      "B. projector",
+      "C. computer",
+      "D. scanner"
+    ]
+  },
+  {
+    numb: 8,
+    question: "ICT skills are about understanding and applying a range of computer programs, ___________, and other applications.",
+    answer: "B. software",
+    options: [
+      "A. hardware",
+      "B. software",
+      "C. peripheral",
+      "D. hard drive"
+    ]
+  },
+  {
+    numb: 9,
+    question: "It is a program which generates slide show presentations.",
+    answer: "B. MS PowerPoint",
+    options: [
+      "A. Microsoft (MS) Word",
+      "B. MS PowerPoint",
+      "C. MS Excel",
+      "D. MS Publisher"
+    ]
+  },
+  {
+    numb: 10,
+    question: "To Copy, press (Ctrl+C); to Paste, press ________ on the computer keyboard.",
+    answer: "D. (Ctrl+V)",
+    options: [
+      "A. (Shift+V)",
+      "B. (Shift+P)",
+      "C. (Ctrl+P)",
+      "D. (Ctrl+V)"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Information and Communication Technology (ICT) is generally accepted to mean all technologies and allow people and organizations to interact in the __________world.",
+    answer: "C. digital",
+    options: [
+      "A. classical",
+      "B. traditional",
+      "C. digital",
+      "D. none of them"
+    ]
+  },
+  {
+    numb: 12,
+    question: "A video clip of a story entitled 'Rich People Problem' is in _______ file format.",
+    answer: "A. mp4",
+    options: [
+      "A. mp4",
+      "B. html",
+      "C. jpeg",
+      "D. xls"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Using interactive digital tools to tell a story is called digital _________.",
+    answer: "D. storytelling",
+    options: [
+      "A. storybrowsing",
+      "B. storymapping",
+      "C. storykeeping",
+      "D. storytelling"
+    ]
+  },
+  {
+    numb: 14,
+    question: "A person employed to drive a private car is called _______.",
+    answer: "C. chauffeur",
+    options: [
+      "A. driver",
+      "B. courier",
+      "C. chauffeur",
+      "D. pauper"
+    ]
+  },
+  {
+    numb: 15,
+    question: "When an item is lacking interesting quality, it has a ______________ label.",
+    answer: "B. nondescript",
+    options: [
+      "A. nonsense",
+      "B. nondescript",
+      "C. no quality",
+      "D. no value"
+    ]
+  },
+  // Add more questions here if needed
+];
